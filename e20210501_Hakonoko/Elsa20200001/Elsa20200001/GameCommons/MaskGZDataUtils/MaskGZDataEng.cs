0a9fdb6caad28dd7c24a5e7e315431b9a3a273ed
soleil-taruto <soleil.taruto@gmail.com>
2021-05-29 13:35:17 +0900

    Voyager 1 is 22882055454.564 kilometers away from the Sun.

--- e20210501_Hakonoko/Elsa20200001/Elsa20200001/GameCommons/MaskGZDataUtils/MaskGZDataEng.cs
+++ e20210501_Hakonoko/Elsa20200001/Elsa20200001/GameCommons/MaskGZDataUtils/MaskGZDataEng.cs
@@ -83,7 +83,7 @@ namespace Charlotte.GameCommons.MaskGZDataUtils
 
 		public void Transpose(byte[] data)
 		{
-			this.Transpose(data, "2020122821"); // 難読化貢献のため seed を文字列化しておく
+			this.Transpose(data, "2021052913"); // 難読化貢献のため seed を文字列化しておく
 		}
 	}
 }
