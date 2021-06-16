a5db5dbfba163d67050897389a3263fbc0d8f4b2
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-06-17 00:42:23 +0900

    Voyager 1 is 22909015465.874 kilometers away from the Sun.

--- e20210621_MSSAGame/Elsa20200001/Elsa20200001/Games/Enemies/Tests/Enemy_B30a430d930f330c80001.cs
+++ e20210621_MSSAGame/Elsa20200001/Elsa20200001/Games/Enemies/Tests/Enemy_B30a430d930f330c80001.cs
@@ -32,7 +32,7 @@ namespace Charlotte.Games.Enemies.Tests
 
 					DDPrint.SetDebug((int)this.X - DDGround.ICamera.X, (int)this.Y - DDGround.ICamera.Y);
 					DDPrint.SetBorder(new I3Color(0, 0, 0));
-					DDPrint.PrintLine("Tests/イベント0001");
+					DDPrint.PrintLine("イベント0001");
 					DDPrint.Reset();
 
 					// 当たり判定無し
