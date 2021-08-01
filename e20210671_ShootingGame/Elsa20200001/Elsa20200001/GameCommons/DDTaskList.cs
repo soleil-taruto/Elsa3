4528412cd091468182d154ba49caaf964ad972d6
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-08-01 21:35:53 +0900

    Voyager 1 is 22975992200.750 kilometers away from the Sun.

---
+++ e20210671_ShootingGame/Elsa20200001/Elsa20200001/GameCommons/DDTaskList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte.GameCommons
+{
+	public class DDTaskList
+	{
+		private DDList<Func<bool>> Tasks = new DDList<Func<bool>>();
+
+		public void Add(Func<bool> task)
+		{
+			this.Tasks.Add(task);
+		}
+
+		public void ExecuteAllTask()
+		{
+			for (int index = 0; index < this.Tasks.Count; index++)
+			{
+				if (!this.Tasks[index]()) // ? 終了
+				{
+					this.Tasks[index] = null;
+				}
+			}
+			this.Tasks.RemoveAll(task => task == null);
+		}
+
+		public void Clear()
+		{
+			this.Tasks.Clear();
+		}
+
+		public int Count
+		{
+			get
+			{
+				return this.Tasks.Count;
+			}
+		}
+
+		// ====
+		// ここから便利機能
+		// ====
+
+		public void Delay(int delayFrame, Action routine)
+		{
+			int endFrame = DDEngine.ProcFrame + delayFrame;
+
+			this.Add(() =>
+			{
+				if (DDEngine.ProcFrame < endFrame)
+					return true;
+
+				routine();
+				return false;
+			});
+		}
+
+		public void Keep(int keepFrame, Action routine)
+		{
+			int endFrame = DDEngine.ProcFrame + keepFrame;
+
+			this.Add(() =>
+			{
+				routine();
+				return DDEngine.ProcFrame < endFrame;
+			});
+		}
+	}
+}
